52a0d0ec246f4baf9432c75a3206eb0c706414d9
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2020-02-05 17:44:54 +0000

    Update AQ.py

--- piAQ/AQ.py
+++ piAQ/AQ.py
@@ -26,21 +26,21 @@ def pm1():
     readings = pms5003.read()
     pm1_reading = (readings.pm_ug_per_m3(1.0))
     pm1_reading = "{:.0f}".format(pm1_reading)
-    return pm1_reading
+    return float(pm1_reading)
     pm1_reading.flush()
 
 def pm25():
     readings = pms5003.read()
     pm25_reading = (readings.pm_ug_per_m3(2.5))
     pm25_reading = "{:.0f}".format(pm25_reading)
-    return pm25_reading
+    return float(pm25_reading)
     pm25_reading.flush()
     
 def pm10():
     readings = pms5003.read()
     pm10_reading = (readings.pm_ug_per_m3(10.0))
     pm10_reading = "{:.0f} ".format(pm10_reading)
-    return pm10_reading
+    return float(pm10_reading)
     pm10_reading.flush()
 
 
